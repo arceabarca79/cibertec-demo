51af71ad00db3a61b91ac378e1e9cbe407c35dbe
Javier Arce <arceabarca@yahoo.es>
2021-10-15 19:45:17 -0500

    Primer Ingreso

---
+++ CibertecDemo.MiPrimeraWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CibertecDemo.MiPrimeraWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
